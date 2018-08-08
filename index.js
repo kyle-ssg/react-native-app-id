96287a3c99e931598427d65d6c1819629a377e3f
Johannes Klein <jo@temka.de>
2018-08-08 19:16:54 +0200

    add display name
    code cleanup, add catch block

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 #!/usr/bin/env node
 
-var newID = process.argv[2];
-var _ = require('lodash');
-var fs = require('fs');
+const newID = process.argv[2];
+const newName = process.argv[3];
+const _ = require('lodash');
+const fs = require('fs');
 
 if (newID && newID.split('.').length <2) {
-    console.log("Please type a valid bundle id e.g. com.solidstategroup.myapp");
+    console.log('Please type a valid bundle id e.g. com.solidstategroup.myapp');
     return;
 }
+
 Promise.all([
-    require('./src/ios')(newID),
-    require('./src/android')(newID)
+    require('./src/ios')(newID, newName),
+    require('./src/android')(newID, newName)
 ])
-    .then(function (items) {
+    .then((items) => {
 
         const ios = items[0];
         const android = items[1];
 
-        _.each(ios,(data, location)=> {
-            console.log("Writing", location);
-            fs.writeFileSync(location,data);
+        _.each(ios, (data, location) => {
+            console.log(`Writing ${location}`);
+            fs.writeFileSync(location, data);
         });
 
-        _.each(android,(data,location)=> {
-            console.log("Writing", location);
-            fs.writeFileSync(location,data);
+        _.each(android, (data, location) => {
+            console.log(`Writing ${location}`);
+            fs.writeFileSync(location, data);
         });
 
-    });
+    })
+    .catch(e => console.log(e));
