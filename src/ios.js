1b5598aff28596568d046e933629a8e760c905f6
Johannes Klein <jo@temka.de>
2018-08-08 19:14:09 +0200

    add display name replace
    code clean up

--- src/ios.js
+++ src/ios.js
@@ -1,50 +1,64 @@
 const fs = require('fs');
 const path = require('path');
-const dir = process.argv[3] || process.cwd();
+// const dir = process.argv[3] || process.cwd();
+const dir = process.cwd();
 
 
-module.exports = (newID) => {
+module.exports = (newID, newName) => {
     return new Promise((resolve, reject) => {
-
-
         const srcpath = path.resolve(dir, 'ios/');
-
-
         let folders;
+
         try {
             folders = fs.readdirSync(srcpath)
                 .filter(file => fs.statSync(path.join(srcpath, file)).isDirectory())
-
         } catch (e) {
-            reject(srcpath, 'directory not found, re you sure you are in a react native project?');
+            reject(`${srcpath} directory not found, are you sure you are in a react native project?`);
         }
 
-
         const folder = folders && folders.find((f) => f.indexOf('.xcodeproj') > -1).replace('.xcodeproj', '');
-        const plist = folders && path.resolve(dir, 'ios/' + folder + '/Info.plist');
-        const pbxproj = folders && path.resolve(dir, 'ios/' + folder + '.xcodeproj/project.pbxproj');
+        const plist = folders && path.resolve(dir, `ios/${folder}/Info.plist`);
+        const pbxproj = folders && path.resolve(dir, `ios/${folder}.xcodeproj/project.pbxproj`);
         let changes = {};
 
-        console.log('IOS: Reading', plist);
+        console.log(`IOS: Reading ${plist}`);
+
         if (!plist) {
             reject('Are you sure you are in a react native project?');
         }
 
         fs.readFile(plist, 'utf8', (err, markup) => {
-            if (err == null) {
-                var searchBundleId = new RegExp("(<key>CFBundleIdentifier</key>\n.*?<string>)(.*?)(</string>)", 'm');
-                var match = markup.match(searchBundleId);
-
-                if (match) {
-                    console.log('IOS: Found iOS bundle ID', match[2]);
-                    changes[plist] = markup.replace(searchBundleId, "$1"+newID+"$3");
-                } else {
-                    console.log("IOS ERROR: Could not find product bundle identifier");
-                }
-                resolve(changes);
-            } else {
+            if (err) {
                 reject('Are you sure you are in a react native project?');
             }
+
+            const searchBundleId = new RegExp("(<key>CFBundleIdentifier</key>\n.*?<string>)(.*?)(</string>)", 'm');
+            const searchDisplayName = new RegExp("(<key>CFBundleDisplayName</key>\n.*?<string>)(.*?)(</string>)", 'm');
+
+            const matchId = markup.match(searchBundleId);
+            const matchName = markup.match(searchDisplayName);
+
+            if (matchId) {
+                if (matchId[2] === '$(PRODUCT_BUNDLE_IDENTIFIER)') {
+                    reject('Please move the Bundle ID into the Info.plist for this to work.');
+                }
+
+                console.log(`IOS: Found iOS bundle ID ${matchId[2]}`);
+                changes[plist] = markup.replace(searchBundleId, "$1" + newID + "$3");
+            } else {
+                reject('IOS ERROR: Could not find product bundle identifier');
+            }
+
+            if (newName) {
+                if (matchName) {
+                    console.log(`IOS: Found iOS display name ${matchName[2]}`);
+                    changes[plist] = changes[plist].replace(searchDisplayName, "$1" + newName + "$3");
+                } else {
+                    reject('IOS ERROR: Could not find display name');
+                }
+            }
+
+            resolve(changes);
         });
     })
 }
